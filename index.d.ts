7caeee9b78e80c135ee7064facc0d21049c8ed7f
Alexandr Korotaev <alexandr.ko@exodus.io>
2022-12-01 14:49:14 +0300

    feat: add second Encode param type

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,4 @@
 
-export function Encode(input: ArrayBuffer): ArrayBuffer
+export function Encode(input: ArrayBuffer, bitLength?: number): ArrayBuffer
 
 export function Decode(input: ArrayBuffer): ArrayBuffer
